c5f44bce259a1cbd2971d2524bfb073fbc20fc32
Alexandru Scvortov <scvalex@gmail.com>
2012-09-10 02:46:04 +0000

    add naive implementation

---
+++ Naive.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import qualified Data.ByteString.Char8 as BS
+import Network.Socket ( Socket, recv )
+import Network.Socket.ByteString ( sendAll )
+import Utils ( runAcceptLoop, bindPort )
+
+main :: IO ()
+main = do
+    lsocket <- bindPort 5000
+    runAcceptLoop lsocket handleRequest
+
+handleRequest :: Socket -> IO ()
+handleRequest socket = do
+    -- FIXME This should be a loop to handle partial receives.
+    requestLine <- recv socket 1024
+    let ("GET" : uri : _) = words requestLine
+    text <- BS.readFile ('.' : uri)
+    sendAll socket text
